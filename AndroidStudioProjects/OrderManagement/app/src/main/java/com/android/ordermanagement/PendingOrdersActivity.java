17b49312eeb16233c72c5bf81075c0c0517d174b
audygit <udaysankar.in@gmail.com>
2017-02-08 01:24:23 +0530

    first

--- AndroidStudioProjects/OrderManagement/app/src/main/java/com/android/ordermanagement/PendingOrdersActivity.java
+++ AndroidStudioProjects/OrderManagement/app/src/main/java/com/android/ordermanagement/PendingOrdersActivity.java
@@ -1,10 +1,15 @@
 package com.android.ordermanagement;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.android.ordermanagement.Models.Customer;
 import com.android.ordermanagement.Models.Order;
 import com.google.gson.Gson;
 
@@ -14,17 +19,36 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class PendingOrdersActivity extends AppCompatActivity {
+public class PendingOrdersActivity extends AppCompatActivity implements CustomerFilterAdapter.HandleSelections{
 
     private ArrayList<Order> pendingOrders=new ArrayList<>();
     private RecyclerView recyclerView;
     private OrdersAdapter ordersAdapter;
+    private ImageButton filters;
+    private DrawerLayout drawerLayout;
+    private RecyclerView customerListView;
+    private ArrayList<Integer> selectedCustomers=new ArrayList<>();
+    private  ArrayList<Customer> customers=new ArrayList<>();
+    private CustomerFilterAdapter filterAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pending_orders);
         recyclerView= (RecyclerView) findViewById(R.id.pending_orders);
         ordersAdapter=new OrdersAdapter(PendingOrdersActivity.this,pendingOrders);
+        filters= (ImageButton) findViewById(R.id.filters);
+        drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        filters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.RIGHT);
+            }
+        });
+        customerListView= (RecyclerView) findViewById(R.id.customers_list);
+        filterAdapter=new CustomerFilterAdapter(PendingOrdersActivity.this,customers);
+        customerListView.setLayoutManager(new LinearLayoutManager(PendingOrdersActivity.this));
+        customerListView.setAdapter(filterAdapter);
+        filterAdapter.setHandleSelections(PendingOrdersActivity.this);
         recyclerView.setLayoutManager(new LinearLayoutManager(PendingOrdersActivity.this));
         recyclerView.setAdapter(ordersAdapter);
         getOrders();
@@ -40,13 +64,13 @@ public class PendingOrdersActivity extends AppCompatActivity {
                     "\"total_amount\":328358.40,\"products\":[{\"id\":5,\"name\":\"AMBICA NEW BANGARAM 75 GRAMS(BOX)\",\"quantity\":10,\"unit\":\"cases\",\"amount\":27825.60}," +
                     "{\"id\":2,\"name\":\"AMBICAATHISAYADHOOP12CONES(BOX)\",\"quantity\":10,\"unit\":\"cases\",\"amount\":492},{\"id\":6,\"name\":\"AmbicaNityaPoojaSparsha(Box)-1Grams\"," +
                     "\"quantity\":70,\"unit\":\"cases\",\"amount\":153568.80},{\"id\":1,\"name\":\"AMBICAMAHALAKSHMIDHOOP44GRAMS\",\"quantity\":40,\"unit\":\"cases\",\"amount\":48882}]," +
-                    "\"service_tax\":12.26,\"vat\":39403},{\"id\": 3,\"cusoomer_id\":078,\"customer_name\":\"MahalakhsmiWholesalers\",\"total_amount\":96929.35," +
+                    "\"service_tax\":12.26,\"vat\":39403},{\"id\": 3,\"cusoomer_id\":75,\"customer_name\":\"MahalakhsmiWholesalers\",\"total_amount\":96929.35," +
                     "\"products\":[{\"id\":7,\"name\":\"AMBICASUBHALAGNAMPOOJA8STICKS\",\"quantity\":5,\"unit\":\"cases\",\"amount\":12411.75}," +
                     "{\"id\":8,\"name\":\"AMBICAJAVVAJI70GRAMS\",\"quantity\":5,\"unit\":\"cases\",\"amount\":7810}," +
                     "{\"id\":1,\"name\":\"AMBICAMAHALAKSHMIDHOOP44GRAMS\",\"quantity\":40,\"unit\":\"cases\",\"amount\":48882}," +
                     "{\"id\":4,\"name\":\"AMBICANEWGULAB70GRAMS\",\"quantity\":10,\"unit\":\"cases\",\"amount\":27825.60}],\"service_tax\":12.26,\"vat\":11631.5}]," +
-                    "\"customers\":[{\"id\":1,\"name\":\"HariProvisionStores\"},{\"id\":1,\"name\":\"AnjaneyaCondiments\"}," +
-                    "{\"id\":1,\"name\":\"MahalakshmiWholesalers\"}]}}");
+                    "\"customers\":[{\"id\":1,\"name\":\"HariProvisionStores\"},{\"id\":23,\"name\":\"AnjaneyaCondiments\"}," +
+                    "{\"id\":75,\"name\":\"MahalakshmiWholesalers\"}]}}");
             JSONObject job= jsonObject.getJSONObject("result");
            JSONArray jarray= job.getJSONArray("orders");
             for (int i=0;i<jarray.length();i++){
@@ -54,12 +78,38 @@ public class PendingOrdersActivity extends AppCompatActivity {
                 Order order=gson.fromJson(jarray.getJSONObject(i).toString(),Order.class);
                 pendingOrders.add(order);
             }
+            JSONArray cusArray=job.getJSONArray("customers");
+            for (int i=0;i<cusArray.length();i++){
+                Gson gson=new Gson();
+                Customer customer=gson.fromJson(cusArray.getJSONObject(i).toString(),Customer.class);
+                customers.add(customer);
+                selectedCustomers.add(customer.getId());
+            }
             ordersAdapter.setOrders(pendingOrders);
             ordersAdapter.notifyDataSetChanged();
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void onFiltersChanged(int id) {
+        if (selectedCustomers.contains(Integer.valueOf(id))){
+            selectedCustomers.remove(Integer.valueOf(id));
+        }else {
+            selectedCustomers.add(Integer.valueOf(id));
+        }
+        ArrayList <Order> orders=new ArrayList<>();
+        for (Order order:pendingOrders){
+            if (selectedCustomers.contains(Integer.valueOf(order.getCustomerId()))){
+                orders.add(order);
+            }else {
+
+            }
+        }
+        ordersAdapter.setOrders(orders);
+        ordersAdapter.notifyDataSetChanged();
     }
 }
 
---
+++ AndroidStudioProjects/OrderManagement/app/src/main/java/com/android/ordermanagement/customerFilterAdapter.java
@@ -0,0 +1,84 @@
+package com.android.ordermanagement;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.android.ordermanagement.Models.Customer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by audyf on 2/7/2017.
+ */
+public class CustomerFilterAdapter extends RecyclerView.Adapter<CustomerFilterAdapter.MyVH> {
+    private Context mContext;
+    private ArrayList<Customer> customers;
+    private ArrayList<Integer> selectedCustomers;
+    private HandleSelections handleSelections;
+    public CustomerFilterAdapter(Context mContext, ArrayList<Customer> customers) {
+        this.mContext = mContext;
+        this.customers = customers;
+    }
+
+    public HandleSelections getHandleSelections() {
+        return handleSelections;
+    }
+
+    public void setHandleSelections(HandleSelections handleSelections) {
+        this.handleSelections = handleSelections;
+    }
+
+    public  interface HandleSelections{
+        public void onFiltersChanged(int id );
+    }
+    @Override
+    public MyVH onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.filter_item, null);
+        MyVH vh = new MyVH(view);
+        return vh;
+    }
+
+    public void setCustomers(ArrayList<Customer> providers) {
+        this.customers = providers;
+    }
+    @Override
+    public void onBindViewHolder(MyVH holder, int position) {
+        Customer provider=customers.get(position);
+        holder.customerName.setText(provider.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return customers.size();
+    }
+
+    public class MyVH extends RecyclerView.ViewHolder {
+        private TextView customerName;
+        private CheckBox checkBox;
+        public MyVH(View convertView) {
+            super(convertView);
+            checkBox= (CheckBox) convertView.findViewById(R.id.check_box);
+            customerName= (TextView) convertView.findViewById(R.id.name);
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (checkBox.isChecked()){
+                        checkBox.setChecked(false);
+                    }else {
+                        checkBox.setChecked(true);
+                    }
+                    Customer customer=customers.get(getAdapterPosition());
+                    handleSelections.onFiltersChanged(customer.getId());
+
+                }
+            });
+        }
+    }
+}
+
